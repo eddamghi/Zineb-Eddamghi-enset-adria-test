def6ce460f06026eb3d21a718de72a0b1795d843
zineb <eddamghizineb@gmail.com>
2023-10-30 09:51:27 +0100

    Question 2: J'ai crée les 3 services

--- config-service/src/main/java/com/eddamghi/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/eddamghi/configservice/ConfigServiceApplication.java
@@ -2,8 +2,13 @@ package com.eddamghi.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+ // This annotation is required to register the service with Eureka
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
